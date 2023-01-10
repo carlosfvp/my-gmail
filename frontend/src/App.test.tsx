ddc3cd987ea44eb704b66b3af2927f19eccfd6aa
Carlos Villacorta <carlos@lawnbuddy.com>
2023-01-10 13:13:49 -0500

    improved UI test cases
    display mailboxes
    send mail and reload mailboxes to show the new received mail
    the mocked handler implements the services logic

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,51 +1,100 @@
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  queryByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import App from "./App";
 import MailboxService from "./services/mailbox.service";
-import { ReportHandler } from "web-vitals";
+import { act } from "react-dom/test-utils";
 // https://github.com/prisma/prisma/issues/8558#issuecomment-1129055580
 global.setImmediate = jest.useRealTimers as unknown as typeof setImmediate;
 
-test("display mailbox", async () => {
-  render(<App />);
-  const mailAddress = screen.getByDisplayValue(/carlos@my-gmail.com/i);
-  mailAddress.setAttribute("value", "carlos1@my-mail.com");
+describe("App test", () => {
+  test("display mailbox and folders", async () => {
+    act(() => {
+      render(<App />);
+    });
+    const mailAddress = screen.getByDisplayValue("carlos@my-gmail.com");
+    mailAddress.setAttribute("value", "carlos1@my-mail.com");
 
-  const loadButton = screen.getByText(/change mailbox/i);
-  loadButton.click();
+    const loadButton = screen.getByText("Change Mailbox");
+    act(() => {
+      loadButton.click();
+    });
 
-  await screen.findByText("Inbox");
-  await screen.findByText("Sent");
-  await screen.findByText("Trash");
-});
+    await screen.findByText("Inbox");
+    await screen.findByText("Sent");
+    await screen.findByText("Trash");
+  });
 
-test("send mail and wait for mailbox", async () => {
-  render(<App />);
-  const mailAddress = screen.getByDisplayValue(/carlos@my-gmail.com/i);
-  const loadButton = screen.getByText(/change mailbox/i);
+  test("send mail and wait for mailbox", async () => {
+    act(() => {
+      render(<App />);
+    });
 
-  mailAddress.setAttribute("value", "carlos2@my-gmail.com");
+    const mailAddress = screen.getByDisplayValue("carlos@my-gmail.com");
+    const loadButton = screen.getByText("Change Mailbox");
 
-  loadButton.click();
+    act(() => {
+      fireEvent.change(mailAddress, {
+        target: { value: "carlos2@my-gmail.com" },
+      });
 
-  await screen.findByText("Inbox");
+      loadButton.click();
+    });
 
-  mailAddress.setAttribute("value", "carlos1@my-gmail.com");
-  loadButton.click();
+    await screen.findByText("Inbox");
 
-  await screen.findByText("Inbox");
+    act(() => {
+      fireEvent.change(mailAddress, {
+        target: { value: "carlos1@my-gmail.com" },
+      });
 
-  const randomId = Math.floor(Math.random() * 100000);
-  const randomSubject = `Subject Test ${randomId}`;
+      loadButton.click();
+    });
 
-  const notPresent = screen.queryByText(randomSubject);
-  expect(notPresent).toBeNull();
+    await waitFor(() => {
+      expect(() => screen.getByText("Not mails in this folder")).toThrow();
+    });
 
-  MailboxService.sendMail(
-    "carlos2@my-gmail.com",
-    "carlos1@my-gmail.com",
-    randomSubject,
-    "Body test 123"
-  );
+    await screen.findByText("Inbox");
 
-  await screen.findByText(randomSubject);
+    const randomId = Math.floor(Math.random() * 100000);
+    const randomSubject = `Subject Test ${randomId}`;
+
+    const notPresent = screen.queryByText(randomSubject);
+    expect(notPresent).toBeNull();
+
+    await act(async () => {
+      await MailboxService.sendMail(
+        "carlos2@my-gmail.com",
+        "carlos1@my-gmail.com",
+        randomSubject,
+        "Body test 123"
+      );
+
+      fireEvent.change(mailAddress, {
+        target: { value: "carlos2@my-gmail.com" },
+      });
+
+      loadButton.click();
+    });
+
+    await screen.findByText("Inbox");
+
+    act(() => {
+      fireEvent.change(mailAddress, {
+        target: { value: "carlos1@my-gmail.com" },
+      });
+
+      loadButton.click();
+    });
+
+    await waitFor(() => {
+      expect(() => screen.getByText(randomSubject)).not.toThrow();
+    });
+  });
 });
