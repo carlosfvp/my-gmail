b2fe1f27bfba7b1dca8ad8d3c1d8547a176038a6
Carlos Villacorta <carlos@lawnbuddy.com>
2023-01-10 18:01:14 -0500

    comments and improving error handling

--- backend/src/mail/mail.controller.ts
+++ backend/src/mail/mail.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { MailService } from './mail.service';
 import { CreateMailDto } from './dto/createMailDto';
-import { MailboxService } from 'src/mailbox/mailbox.service';
-import { EventsGateway } from '../events/events.gateway';
+import { MailboxService } from '../mailbox/mailbox.service';
 
 @Controller('mail')
 export class MailController {
@@ -11,24 +19,49 @@ export class MailController {
     private readonly mailService: MailService,
   ) {}
 
+  /**
+   * Method to create a mail, includes the same message
+   * in sent of the sender and inbox folder for the receiver.
+   * @param createMailDto
+   */
   @Post()
-  async create(@Body() createMailDto: CreateMailDto) {
-    // TODO project only ids
-    const toMailbox = await this.mailboxService.getMailboxByOwner(
-      createMailDto.to,
-    );
+  async create(@Body() createMailDto: CreateMailDto): Promise<boolean> {
+    // TODO project only ids on the mongoose query
+    try {
+      const toMailbox = await this.mailboxService.getMailboxByOwner(
+        createMailDto.to,
+      );
 
-    const fromMailbox = await this.mailboxService.getMailboxByOwner(
-      createMailDto.from,
-    );
+      const fromMailbox = await this.mailboxService.getMailboxByOwner(
+        createMailDto.from,
+      );
 
-    await this.mailService.createMail(
-      toMailbox._id.toString(),
-      fromMailbox._id.toString(),
-      createMailDto,
-    );
+      await this.mailService.createMail(
+        toMailbox._id.toString(),
+        fromMailbox._id.toString(),
+        createMailDto,
+      );
+    } catch (error) {
+      const e = new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Error creating a mail',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+      e.cause = error;
+      throw e;
+    }
+
+    return true;
   }
 
+  /**
+   * Method to delete mail using params
+   * @param mailboxId
+   * @param folderId
+   * @param mailId
+   */
   @Delete(':mailboxId/:folderId/:mailId')
   async delete(
     @Param() mailboxId: string,
@@ -43,9 +76,13 @@ export class MailController {
     return deletedCount === 1;
   }
 
+  /**
+   * TODO: implement method to move mail to a folder
+   * @param createMailDto
+   */
   @Post('move')
   async moveToFolder(@Body() createMailDto: CreateMailDto) {
-    // TODO: implement method
+    //
     throw new Error('Method not implemented');
   }
 }
--- backend/src/mailbox/mailbox.controller.ts
+++ backend/src/mailbox/mailbox.controller.ts
@@ -1,31 +1,58 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { MailboxService } from './mailbox.service';
-import { Mailbox, MailboxDocument } from 'src/mailbox/schemas/mailbox.schema';
 import { CreateMailboxDto } from './dto/createMailboxDto';
-import { Folder } from './schemas/folder.schema';
-import { Mail } from '../mail/schemas/mail.schema';
-import { MailService } from 'src/mail/mail.service';
-import { EventsGateway } from '../events/events.gateway';
+import { MailboxDocument } from './schemas/mailbox.schema';
 
 @Controller('mailbox')
 export class MailboxController {
-  constructor(
-    private readonly mailboxService: MailboxService,
-    private readonly mailService: MailService,
-  ) {}
+  constructor(private readonly mailboxService: MailboxService) {}
 
+  /**
+   * Method to create mailbox for new user
+   * @param createMailboxDto
+   */
   @Post()
   async create(@Body() createMailboxDto: CreateMailboxDto) {
     await this.mailboxService.createMailbox(createMailboxDto);
   }
 
+  /**
+   * Method to get mailbox DTO, creates mailbox if it doesn't exist
+   * @param owner
+   * @returns
+   */
   @Get(':owner')
-  async getMailboxId(@Param('owner') owner: string): Promise<Mailbox> {
-    let mb: Mailbox = await this.mailboxService.getMailboxByOwner(owner);
+  async getMailbox(@Param('owner') owner: string): Promise<MailboxDocument> {
+    let returnMb: MailboxDocument = null;
+    try {
+      let mb: MailboxDocument = await this.mailboxService.getMailboxByOwner(
+        owner,
+      );
 
-    if (mb == null)
-      mb = await this.mailboxService.createMailbox({ owner: owner });
+      if (mb == null)
+        mb = await this.mailboxService.createMailbox({ owner: owner });
 
-    return mb;
+      return mb;
+    } catch (error) {
+      const e = new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Error getting mailbox info',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+      e.cause = error;
+      throw e;
+    }
+
+    return returnMb;
   }
 }
